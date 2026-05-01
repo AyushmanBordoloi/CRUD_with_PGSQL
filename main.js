e8d2424291ae68c4de85ca06db99498589d23c7d
AyushmanBordoloi <ayushmanbordoloi02@gmail.com>
2026-05-01 21:03:26 +0530

    Add PUT operation (update data)

--- main.js
+++ main.js
@@ -53,6 +53,20 @@ app.get('/fetchByID/:id', (req, res)=> {
     })
 })
 
+app.put('/update/:id', (req, res)=>{
+    const id = req.params.id;
+    const name = req.body.name;
+    const update_query = "update demo_table set name = $1 where id = $2"
+    conn.query(update_query, [name, id], (err, result)=>{
+        if(err){
+            res.send(err)
+        }else{
+            console.log(result)
+            res.send("UPDATED")
+        }
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("Server is running on port: 3000")
 })
